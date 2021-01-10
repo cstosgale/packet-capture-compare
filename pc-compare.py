516414cf40219f0cbb3c0f21e782d54a4e9d56ad
Chris Stos-Gale <chris@stos-gale.com>
2021-01-10 00:12:23 +0000

    Implemented XLSX generation

--- pc-compare.py
+++ pc-compare.py
@@ -2,7 +2,7 @@ import csv
 import json
 import sys
 import datetime
-#import XlsXWriter
+import xlsxwriter
 from os import listdir
 from jsonextract import json_extract
 from collections import OrderedDict 
@@ -14,7 +14,7 @@ clientjson = 'client capture.json'
 serverjson = 'firewall capture.json'
 wbname = 'cap-analysis.xlsx'
 clientconcat_list = []
-cap_schema = ['frame.time', 'ip.src', 'ip.dst', 'tcp.srcport', 'tcp.dstport', 'tcp.seq', 'tcp.nxtseq', 'tcp.flags.syn', 'tcp.flags.ack', 'tcp.flags.ack', 'tcp.flags.reset']
+cap_schema = ['frame.time', 'ip.src', 'ip.dst', 'tcp.srcport', 'tcp.dstport', 'tcp.seq', 'tcp.nxtseq', 'tcp.flags.syn', 'tcp.flags.ack', 'tcp.flags.reset']
 
 def ftime_datetime(string):
 	#Converts the frame.time string formatted as "Jan  9, 2021 11:12:52.206763000 GMT Standard Time" to datetime
@@ -44,44 +44,71 @@ def import_cap(path, filename):
 			index += 1
 			if len(cap_dict) < index:
 				cap_dict[index] = {}
-			cap_dict[index][sitem] = jitem
+			if sitem == 'frame.time':
+				cap_dict[index][sitem] = ftime_datetime(jitem)
+			else:
+				cap_dict[index][sitem] = jitem
 	return cap_dict
 
 def cap_concat(cap_dict, index):
 	# Concatenate the unique packet values from the dictionary
 	concat = cap_dict[index]['tcp.srcport'] + cap_dict[index]['tcp.dstport'] + cap_dict[index]['tcp.seq'] + cap_dict[index]['tcp.nxtseq'] + cap_dict[index]['tcp.flags.syn'] + cap_dict[index]['tcp.flags.ack'] + cap_dict[index]['tcp.flags.reset']
 	return concat
+
+def write_xlsx(cap_dict):
+	headers_dict = {}
 	
-# def write_xlsx(wbname):
-# 	# Create a workbook and add a worksheet.
-# 	workbook = xlsxwriter.Workbook(wbname)
-# 	worksheet = workbook.add_worksheet()
-# 
-# 	# Start from the first cell. Rows and columns are zero indexed.
+	# Add a worksheet.
+	worksheet = workbook.add_worksheet()
+	
+	# Start from the first cell. Rows and columns are zero indexed.
+	lastrow = len(cap_dict)
+	lastcolumn = len(cap_schema) - 1
+	
+	#Creates a list of dictionaries containing the headers for the XLSX table, and applies any required formatting
+	datetime_format = workbook.add_format()
+	datetime_format.set_num_format('dd/mm/yyyy hh:mm')
+	for index in range(len(cap_schema)):
+		headers_dict[index] = {}
+		headers_dict[index]['header'] = cap_schema[index]
+		if cap_schema[index] == 'frame.time':
+			headers_dict[index]['format'] = datetime_format
+#	print(headers_dict)
+	data = [list(col) for col in zip(*[d.values() for d in cap_dict])]
+# 	for index in range(len(cap_dict)):
+# 		if index != 0:
+# 			for item in cap_schema:
+# 				worksheet.write(row, col, cap_dict[index][item])		
+# 	# Adds a table and writes out the headers
+	worksheet.add_table(0, 0, lastrow, lastcolumn, {'data': data, 'columns': headers_dict})
+
+	# Write out the headers - this has to be done after the table formatting so it doesn't get overwritten
 # 	row = 0
 # 	col = 0
-# 
-# 	# Iterate over the data and write it out row by row.
-# 	for item, cost in (expenses):
+# 	for item in cap_schema:
 # 		worksheet.write(row, col, item)
-# 		worksheet.write(row, col + 1, cost)
-# 		row += 1
-
-# Write a total using a formula.
-# worksheet.write(row, 0, 'Total')
-# worksheet.write(row, 1, '=SUM(B1:B4)')
-# 
-# workbook.close()
+# 		col += 1
+	# Write out the data
+	row = 1
+	col = 0
+	for index in range(len(cap_dict)):
+		if index != 0:
+			for item in cap_schema:
+				worksheet.write(row, col, cap_dict[index][item])
+				col += 1
+			col = 0
+			row += 1
 
 #Import the captures into two dictionaries
+print('Importing captures')
 clientcap_dict = import_cap(path, clientjson)
 servercap_dict = import_cap(path, serverjson)
 
-
+#Perform the comparison between the two captures
+print('Comparing captures')
 compcap_dict = clientcap_dict
 
-print(clientcap_dict[1])
-
+#Mark the packets first based on if the client packets exist in the server capture, and then the other way round
 for clientindex in range(len(clientcap_dict)):
 	if clientindex != 0:
 		clientconcat = cap_concat(clientcap_dict, clientindex)
@@ -119,23 +146,15 @@ for serverindex in range(len(servercap_dict)):
 compcap_dict = sorted(compcap_dict.values(), key=itemgetter('frame.time'))
 
 
-for index in range(len(compcap_dict)):
-	if compcap_dict[index]['packetatsvr'] == 'Yes':
-		print(compcap_dict[index])
+# for index in range(len(compcap_dict)):
+# 	if compcap_dict[index]['packetatsvr'] == 'Yes':
+# 		print(compcap_dict[index])
 	
-
-
-#print files
-
-#csvfile = csv.writer(open("results.csv", "w+"))
-
-
-# Write CSV Header, If you dont need that, remove this line
-#csvfile.writerow(["clientIP", "DestIP", "clientPort", "serverPort", "Seq", "Nxtseq", "Syn", "Ack", "Rst"])
-
-#print ["Account", "Alias", "Origin"]
-
-#for filename in files:
-
-	
-#	account = filename.split(".")[0]
+#Create the XLSX file and write the data
+print('Writing the output to ', wbname)
+workbook = xlsxwriter.Workbook(wbname)
+#Add the additional fields needed in the scema for the output
+cap_schema.append('packetatclient')
+cap_schema.append('packetatsvr')
+write_xlsx(compcap_dict)
+workbook.close()
